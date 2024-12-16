88ecc77fa14d20c80b9307400cdecee38de11e12
Elena <qa2301-mail.ru>
2024-12-16 23:09:48 +0300

    Change StatisticsServiceTest file

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -5,12 +5,26 @@ import org.junit.jupiter.api.Test;
 
 public class StatisticsServiceTest {
 
+    // Поиск максимального значения
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // Проверка работы метода при наличии одинаковых значений в массиве.
+    @Test
+    void sameValues() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 33, 33, 11, 11, 12, 33};
+        long expected = 33;
 
         long actual = service.findMax(incomesInBillions);
 
